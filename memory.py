667198e0f74cd1bf2fd43d831937ea1c5b1cfed1
davidbison <davidbisonhubert@gmail.com>
2016-02-26 10:06:59 -0800

    Iterate through Memory deck and draw text on canvas

--- memory.py
+++ memory.py
@@ -3,12 +3,18 @@
 import simplegui
 import random
 
+# global state
+FRAME_WIDTH = 800
+FRAME_HEIGHT = 100
+CARD_FONT_SIZE = 56
+CARD_COLOR = "orange"
+
 # helper function to initialize globals
 def new_game():
-  global deck
-  set1 = range(9)
-  set2 = range(9)
-  deck = set1 + set2
+    global deck
+    set1 = range(9)
+    set2 = range(9)
+    deck = set1 + set2
 
 
 # define event handlers
@@ -19,11 +25,18 @@ def mouseclick(pos):
 
 # cards are logically 50x100 pixels in size
 def draw(canvas):
-    pass
+    card_posX = 12
+    card_posY = FRAME_HEIGHT - (CARD_FONT_SIZE / 2 + 4)
+    for card in deck:
+        canvas.draw_text(str(card),
+                         [card_posX, card_posY],
+                         CARD_FONT_SIZE,
+                         CARD_COLOR)
+        card_posX += 100 - 56
 
 
 # create frame and add a button and labels
-frame = simplegui.create_frame("Memory", 800, 100)
+frame = simplegui.create_frame("Memory", FRAME_WIDTH, FRAME_HEIGHT)
 frame.add_button("Reset", new_game)
 label = frame.add_label("Turns = 0")
 
